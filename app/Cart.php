96327cc7d31f8e75880e0884087181c88ecd30ff
oxyage <oxyage@yandex.ru>
2020-04-16 12:22:46 +0300

    Fix model fillable properties

--- app/Cart.php
+++ app/Cart.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     //
+	protected $fillable = ['pizzas', 'customer_name'];
 }
--- app/Pizza.php
+++ app/Pizza.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Pizza extends Model
 {
     //
+	protected $fillable = [
+						'title', 
+						'description',
+						'images',
+						'cost',
+						'weight'
+						];
 }
