feb699f6b6fccdbda79e6ae84117d2ed4de14ed5
oxyage <oxyage@yandex.ru>
2020-04-17 09:06:54 +0300

    removed unnecessary

--- app/Pizza.php
+++ app/Pizza.php
@@ -4,6 +4,9 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static where(string $string, string $string1, int $min)
+ */
 class Pizza extends Model
 {
     //
@@ -17,10 +20,4 @@ class Pizza extends Model
 
 
     public $timestamps = false;
-
-	public function getPizzasByCost($min, $max)
-	{
-
-	}
-
 }
