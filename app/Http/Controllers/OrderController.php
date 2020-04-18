5991f57c0501584f7f16744a4ba59f80c9f0169a
oxyage <oxyage@yandex.ru>
2020-04-18 08:19:34 +0300

    Add take method for test

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,27 +4,42 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Order;
+use App\Cart;
 
 
 class OrderController extends Controller
 {
     //
-	
-	public function make(Request $request)
-	{
-		
-	}
-	
-	public function edit(Request $request, $order_id)
-	{
-		//edit
-	
+
+	public function take(Request $request, $customer_id)
+    {
+        //find in cart all where customer_id
+        // move it to order
+        $customers_cart = Cart::all()->where('customer_id', $customer_id);
+
+        //get last order id
+        $order_id = time();
+
+        foreach($customers_cart as $cart){
+            Order::create([
+                'order_id' => $order_id,
+                'customer_id' => $customer_id,
+                'pizza_id' => $cart['pizza_id'],
+                'count' => $cart['count'],
+                'delivery' => $request->get('delivery'),
+                'contacts' => $request->get('contacts')
+            ]);
+        }
+
+
 	}
 
-	public function remove($order_id)
+	public function history($customer_id)
 	{
-		//not necessary
+       //find all where customer id
+
 	}
-	
-	
+
+
+
 }
